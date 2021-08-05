96b4759f1a277b349927aece6c6d8a4410fa7d07
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-08-05 09:43:23 -0700

    corrected error in calendarId param

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -7,7 +7,7 @@ const credentials = {
   client_id: process.env.CLIENT_ID,
   project_id: process.env.PROJECT_ID,
   client_secret: process.env.CLIENT_SECRET,
-  calendar_id: process.env.CALENDER_ID,
+  calendar_id: process.env.CALENDAR_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
