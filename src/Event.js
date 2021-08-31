726302ee9e1b5c07ac1818e69cef39450d69d9eb
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-08-31 13:33:10 -0700

    add conditional to render creator name if there is one

--- src/Event.js
+++ src/Event.js
@@ -11,39 +11,46 @@ class Event extends Component {
 
     return (
         <div className="expandable">
-           <div className="default">
+          <div className="default">
             <h2 className="eventname">{event.summary}</h2>
             <div>
               <span className="times">{event.start.dateTime}</span> 
               <span className="timezone"> ({event.start.timeZone})</span>
             </div> 
             {/*<p className="times">{event.end.dateTime}</p>*/}
-            <span className="group">@{event.creator.name}</span>
-            <span className="location"> | {event.location}</span>
-           </div> 
-        <div className="expandButton">
-            <button 
-              className="show"
-              onClick={() => this.setState( { expanded: true })}  
+            {(event.creator.name)
+              ? <span className="group">@{event.creator.name} | </span>
+              : null}
+              <span className="location">{event.location}</span>
+          </div> 
+          
+          {this.state.expanded === false
+          ? <div className="expandButton">
+              <button 
+                className="show"
+                onClick={() => this.setState( { expanded: true })}  
               >Show Details
-            </button>
-        </div>
-         {this.state.expanded
-          ? <div className="expanded"> 
-            <div className="descriptions">{event.description}</div>           
-            <div className="links">{event.htmlLink}</div>
-            <div className="attendees">{event.currentAttendees} are currently attending</div>
-
-            <button 
-              className="hide"
-              onClick={() => this.setState( { expanded: false })}
-            >Hide Details
-            </button>
-          </div>
+              </button>
+            </div>
+          : null}
+         
+          {this.state.expanded
+            ? <div className="expanded"> 
+                <div className="descriptions">{event.description}</div>           
+                <div className="links">{event.htmlLink}</div>
+                <div className="attendees">{event.currentAttendees} are currently attending</div>
+                <div className="collapseButton">
+                  <button 
+                    className="hide"
+                    onClick={() => this.setState( { expanded: false })}
+                    >Hide Details
+                  </button>
+                </div>   
+              </div>
           : null}
         </div>
-    )
+      )
+    }
   }
-}
   
 export default Event;
